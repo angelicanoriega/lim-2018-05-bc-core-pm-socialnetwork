e68a3010e768ddd6720fdac907401f50ce521f21
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-07-11 15:39:36 -0500

    Configuración básica del test para funcionamiento

---
+++ src/network.js
@@ -0,0 +1,2 @@
+window.social = (output) => {
+};
---
+++ test/network.spec.js
@@ -0,0 +1,5 @@
+describe('prueba', () => {
+    it('debería exponer función social en objeto global', () => {
+        assert.isFunction(social);
+    });
+})

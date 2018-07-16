75e972456f9a2f8f3f4aec8f3ff3d18387c04b17
Maria <mariaanguevara@gmail.com>
2018-07-16 16:10:53 -0500

    agregando a elementos html  funciona al registrarse con gmail y google

--- src/app.js
+++ src/app.js
@@ -11,95 +11,91 @@ const lastNameRegister = document.getElementById("lastName-register");
 const emailRegister = document.getElementById("email-register");
 const passwordRregister = document.getElementById("password-register");
 //varibles creadas para la visualizacion usando el css en html
-const login=document.getElementById("menu");
-const logout=document.getElementById("singIn-register");
+const login = document.getElementById("menu");
+const logout = document.getElementById("singIn-register");
 //botones de iniciar secion con google y facebook
-const btnGoogle=document.getElementById("google-SignIn");
-const btnFacebook=document.getElementById("facebook-SignIn");
+const btnGoogle = document.getElementById("google-SignIn");
+const btnFacebook = document.getElementById("facebook-SignIn");
 
 
 //confirma que el usuario esta logueando para que no tenga que volver a ingresar sus datos
 window.onload = () => {
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
       console.log('Inicio Logueado ')
-        login.classList.remove("contenedor-menu");
-        logout.classList.add("contenedor-menu");
-        
-      } else {
-        console.log('No esta logueado');
-        login.classList.add("contenedor-menu");
-        logout.classList.remove("contenedor-menu");
-       
-      }
-    });
-  }
+      login.classList.remove("contenedor-menu");
+      logout.classList.add("contenedor-menu");
+
+    } else {
+      console.log('No esta logueado');
+      login.classList.add("contenedor-menu");
+      logout.classList.remove("contenedor-menu");
+
+    }
+  });
+}
 
 //Registrando usuarios nuevos
-btnRegister.addEventListener("click",()=>{
-firebase.auth().createUserWithEmailAndPassword(emailRegister.value, passwordRregister.value)
-.then(()=>{
-           console.log("me registro");
-           console.log(passwordRregister.value);
-           console.log(emailRegister.value);
-          })
-.catch((error)=> {
-                 // Handle Errors here.
-                 let errorCode = error.code;
-                 let errorMessage = error.message;
-                 // ...
-                 });
+btnRegister.addEventListener("click", () => {
+  firebase.auth().createUserWithEmailAndPassword(emailRegister.value, passwordRregister.value)
+    .then(() => {
+      console.log("me registro");
+      console.log(passwordRregister.value);
+      console.log(emailRegister.value);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      // ...
+    });
 })
 
 //Acceso de usuarios existentes
-btnSignIn.addEventListener("click",()=>{
-firebase.auth().signInWithEmailAndPassword(email.value, password.value)
-.then(()=>{
-           console.log("entre");
-           console.log(passwordRregister.value);
-           console.log(emailRegister.value);
-          })
-.catch((error)=> {
-                 // Handle Errors here.
-                 let errorCode = error.code;
-                 let errorMessage = error.message;
-                 // ...
-                 });
+btnSignIn.addEventListener("click", () => {
+  firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+    .then(() => {
+      console.log("entre");
+      console.log(passwordRregister.value);
+      console.log(emailRegister.value);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      // ...
+    });
 
 })
 
 //salir de la cuenta del usuario
 btnLogout.addEventListener('click', () => {
-firebase.auth().signOut()
-.then(()=> {
-            console.log('Cerro Sesión');
-            login.classList.remove("contenedor-menu");
-            logout.classList.add("contenedor-menu");
-           })
-.catch((error)=> {
-                  console.log('Error al cerrar Sesión');
-                 });
+  firebase.auth().signOut()
+    .then(() => {
+      console.log('Cerro Sesión');
+      login.classList.remove("contenedor-menu");
+      logout.classList.add("contenedor-menu");
+    })
+    .catch((error) => {
+      console.log('Error al cerrar Sesión');
+    });
 })
 
-btnGoogle.addEventListener("click",()=>{
+btnGoogle.addEventListener("click", () => {
 
-let provider = new firebase.auth.GoogleAuthProvider();
+  let provider = new firebase.auth.GoogleAuthProvider();
 
-firebase.auth().signInWithPopup(provider)
-.then((result)=> {
-                 console.log("ingrese con google");
-                 // This gives you a Google Access Token. You can use it to access the Google API.
-                 var token = result.credential.accessToken;
-                 // The signed-in user info.
-                 var user = result.user;
-                 // ...
-                 })
-.catch((error)=> {
-                
-                 console.log(error.code);
-                 console.log(error.message);
-                 console.log(error.email);
-                 console.log(error.credential);
+  firebase.auth().signInWithPopup(provider)
+    .then((result) => {
+      console.log("ingrese con google");
 
-                });
+    })
+    .catch((error) => {
+
+      console.log(error.code);
+      console.log(error.message);
+      console.log(error.email);
+      console.log(error.credential);
+
+    });
 })

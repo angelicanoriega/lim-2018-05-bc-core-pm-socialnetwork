8520b7b6426d703989245c7eee7a66a15445be6f
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-07-23 08:44:33 -0500

    Almacenar datos y publicando posts

--- src/app.js
+++ src/app.js
@@ -19,20 +19,25 @@ const btnReturn = document.getElementById("return");
 //varibles creadas para la visualizacion usando el css en html
 const login = document.getElementById("menu");
 const logout = document.getElementById("singIn-register");
-let userName = document.getElementById("user_name");
+const userName = document.getElementById("user_name");
 const register = document.getElementById("register");
 const singIn = document.getElementById("singIn");
-const visualImgFont=document.getElementById("imagen");
+const visualImgFont = document.getElementById("imagen");
+const wall = document.getElementById('wall')
+const dataBase = document.getElementById('dataBase');
+const btnSave = document.getElementById('btnSave');
+const post = document.getElementById('post');
+const posts = document.getElementById('publications');
 
 //ocultando y apareciondo el area de inciar sesion y registro
 btnNewAccount.addEventListener("click", () => {
   register.removeAttribute("class");
   singIn.setAttribute("class", "hidden");
 })
+
 btnReturn.addEventListener("click", () => {
   register.setAttribute("class", "hidden");
   singIn.removeAttribute("class");
-
 })
 
 // condicionales de validacion
@@ -41,124 +46,112 @@ const validationMessageSI = document.getElementById("validation-Message");
 /*mensajes de validacion de Iniciar sesion
 1.- input para introduccion de correo*/
 email.addEventListener("keyup", () => {
-   if (email.value.length <= 0) {
+  if (email.value.length <= 0) {
     validationMessageSI.innerHTML = "<span>Completa el cuadro <strong>Email</strong></span>";
     email.setAttribute("class", "warning");
-   }
-   else{
+  }
+  else {
     validationMessageSI.innerHTML = "<span></span>";
     email.removeAttribute("class");
- 
-   }
+  }
 })
+
 /*2.- input para introduccion de contraseña*/
 password.addEventListener("keyup", () => {
   if (password.value.length <= 0) {
-   validationMessageSI.innerHTML = "<span>Completa el cuadro <strong>Pasword</strong></span>";
-   password.setAttribute("class", "warning");
+    validationMessageSI.innerHTML = "<span>Completa el cuadro <strong>Pasword</strong></span>";
+    password.setAttribute("class", "warning");
   }
-  else{
-   validationMessageSI.innerHTML = "<span></span>";
-   password.removeAttribute("class");
+  else {
+    validationMessageSI.innerHTML = "<span></span>";
+    password.removeAttribute("class");
 
   }
 })
+
 /*constante para escribir el mensaje de validacion en registro*/
 const validationMessage = document.getElementById("validation-message");
 /*mensajes de validacion del registro
 1.- input para introduccion de nombre completo*/
-nameRegister.addEventListener("mousemove", ()=>{
- validationMessage.innerHTML = "<span>Completa este cuadro con tu nombre completo</span>";
+nameRegister.addEventListener("mousemove", () => {
+  validationMessage.innerHTML = "<span>Completa este cuadro con tu nombre completo</span>";
 })
 nameRegister.addEventListener("keyup", () => {
   if (nameRegister.value.length <= 0) {
     validationMessage.innerHTML = "<span>Completa el cuadros de registro <strong>Nombre Completo</strong></span>";
     nameRegister.setAttribute("class", "warning");
   }
-  else{
+  else {
     validationMessage.innerHTML = "<span></span>";
     nameRegister.removeAttribute("class");
 
   }
 })
 /*2.- input para introduccion de nombre de usuario*/
-nickNameRegister.addEventListener("mousemove", ()=>{
+nickNameRegister.addEventListener("mousemove", () => {
   validationMessage.innerHTML = "<span>Completa este cuadro con un nombre de usuario a eleccion </span>";
- })
- nickNameRegister.addEventListener("keyup", () => {
-   if (nickNameRegister.value.length <= 0) {
-     validationMessage.innerHTML = "<span>Completa el cuadro de registro <strong>Nombre de Usuario</strong></span>";
-     nickNameRegister.setAttribute("class", "warning");
-    }
-   else{
-     validationMessage.innerHTML = "<span></span>";
-     nickNameRegister.removeAttribute("class");
-   }
- })
+})
+nickNameRegister.addEventListener("keyup", () => {
+  if (nickNameRegister.value.length <= 0) {
+    validationMessage.innerHTML = "<span>Completa el cuadro de registro <strong>Nombre de Usuario</strong></span>";
+    nickNameRegister.setAttribute("class", "warning");
+  }
+  else {
+    validationMessage.innerHTML = "<span></span>";
+    nickNameRegister.removeAttribute("class");
+  }
+})
 /*3.- input para introduccion de email*/
-emailRegister.addEventListener("mousemove", ()=>{
+emailRegister.addEventListener("mousemove", () => {
   validationMessage.innerHTML = "<span>Completa este cuadro con un email </span>";
- })
- emailRegister.addEventListener("keyup", () => {
-   if (emailRegister.value.length <= 0) {
-     validationMessage.innerHTML = "<span>Completa el cuadro de registro <strong>Email</strong></span>";
-     emailRegister.setAttribute("class", "warning");
-   }
-   else{
-     validationMessage.innerHTML = "<span></span>";
-     emailRegister.removeAttribute("class");
-   }
- })
+})
+emailRegister.addEventListener("keyup", () => {
+  if (emailRegister.value.length <= 0) {
+    validationMessage.innerHTML = "<span>Completa el cuadro de registro <strong>Email</strong></span>";
+    emailRegister.setAttribute("class", "warning");
+  }
+  else {
+    validationMessage.innerHTML = "<span></span>";
+    emailRegister.removeAttribute("class");
+  }
+})
 /*4.- input para introduccion de contraseña*/
-passwordRregister.addEventListener("mousemove", ()=>{
+passwordRregister.addEventListener("mousemove", () => {
   validationMessage.innerHTML = "<span>Completa este cuadro con una contraseña a elección </span>";
- })
- passwordRregister.addEventListener("keyup", () => {
-   if (passwordRregister.value.length === 0) {
-     validationMessage.innerHTML = "<span>Completa el cuadro de registro <strong>Password</strong></span>";
-     passwordRregister.setAttribute("class", "warning");
-   }
-   else if(passwordRregister.value.length<=5){
-    validationMessage.innerHTML = "<span>Completa el cuadro de registro <strong>Password</strong> con una contraseña de minimo 6 digitos</span>"; 
+})
+passwordRregister.addEventListener("keyup", () => {
+  if (passwordRregister.value.length === 0) {
+    validationMessage.innerHTML = "<span>Completa el cuadro de registro <strong>Password</strong></span>";
     passwordRregister.setAttribute("class", "warning");
-   }
-   else{
-     validationMessage.innerHTML = "<span></span>";
-     passwordRregister.removeAttribute("class");
-   }
- })
-
+  }
+  else if (passwordRregister.value.length <= 5) {
+    validationMessage.innerHTML = "<span>Completa el cuadro de registro <strong>Password</strong> con una contraseña de minimo 6 digitos</span>";
+    passwordRregister.setAttribute("class", "warning");
+  }
+  else {
+    validationMessage.innerHTML = "<span></span>";
+    passwordRregister.removeAttribute("class");
+  }
+})
 
 //constante para almacenar datos
-let users = {
+const users = {
   name: "",
-  nickName: "",
-  email: "",
-  password: "",
+  displayName: "",
+  email: ""
 };
-let usersWhitGoogle = {
-  name: "",
-  nickName: "",
-  email: "",
-  password: "",
-};
-
 
 //confirma que el usuario esta logueando para que no tenga que volver a ingresar sus datos
 window.onload = () => {
-  firebase.auth().onAuthStateChanged(function (user) {
+  firebase.auth().onAuthStateChanged(user => {
     if (user) {
-
       userName.innerHTML = `Bienvenid@  ${user.displayName}`;
       console.log(user);
-
-
       console.log('Inicio Logueado ')
       login.classList.remove("hidden");
       logout.classList.add("hidden");
       visualImgFont.setAttribute("class", "hidden");
-
-
+      wall.classList.remove("hidden");
     } else {
       console.log('No esta logueado');
       login.classList.add("hidden");
@@ -166,21 +159,49 @@ window.onload = () => {
       register.setAttribute("class", "hidden");
       singIn.removeAttribute("class");
       visualImgFont.removeAttribute("class");
-
+      wall.classList.add("hidden");
     }
   });
+}
+
+const writeUserData = (userId, name, nickName, email, imageUrl) => {
+  firebase.database().ref('users/' + userId).set({
+    userName: name,
+    userNickName: nickName,
+    email: email,
+    profile_picture: imageUrl
+  });
+}
+
+const writeNewPost = (uid, body) => {
+  // A post entry.
+  const postData = {
+    uid: uid,
+    body: body,
+  };
+
+  // Get a key for a new Post.
+  const newPostKey = firebase.database().ref().child('posts').push().key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/posts/' + newPostKey] = postData;
+  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+  firebase.database().ref().update(updates);
+  return newPostKey;
 }
 
 //Registrando usuarios nuevos
 btnRegister.addEventListener("click", () => {
   firebase.auth().createUserWithEmailAndPassword(emailRegister.value, passwordRregister.value)
-    .then(() => {
-
-
-
+    .then((result) => {
       console.log("me registro");
-      console.log(users);
-
+      const user = result.user;
+      user.displayName = nickNameRegister.value;
+      user.name = nameRegister.value;
+      console.log(nickNameRegister.value);
+      writeUserData(user.uid, user.displayName, user.displayName, user.email, user.photoURL);
     })
     .catch((error) => {
       // Handle Errors here.
@@ -204,7 +225,58 @@ btnSignIn.addEventListener("click", () => {
       let errorMessage = error.message;
       // ...
     });
+})
 
+btnSave.addEventListener('click', () => {
+  var userId = firebase.auth().currentUser.uid;
+  const newPost = writeNewPost(userId, post.value );
+
+
+  var btnUpdate = document.createElement("input");
+  btnUpdate.setAttribute("value", "Update");
+  btnUpdate.setAttribute("type", "button");
+  var btnDelete = document.createElement("input");
+  btnDelete.setAttribute("value", "Delete");
+  btnDelete.setAttribute("type", "button");
+  var contPost = document.createElement('div');
+  var textPost = document.createElement('textarea')
+  textPost.setAttribute("id", newPost);
+
+  textPost.innerHTML = post.value;
+
+  btnDelete.addEventListener('click', () => {
+
+    firebase.database().ref().child('/user-posts/' + userId + '/' + newPost).remove();
+    firebase.database().ref().child('posts/' + newPost).remove();
+
+    while(posts.firstChild) posts.removeChild(posts.firstChild);
+
+    alert('The user is deleted successfully!');
+    reload_page();
+
+  });
+
+  btnUpdate.addEventListener('click', () => {
+    const newUpdate = document.getElementById(newPost);
+    const nuevoPost = {
+      body: newUpdate.value,
+    };
+
+    var updatesUser = {};
+    var updatesPost = {};
+
+    updatesUser['/user-posts/' + userId + '/' + newPost] = nuevoPost;
+    updatesPost['/posts/' + newPost ] = nuevoPost;
+
+    firebase.database().ref().update(updatesUser);
+    firebase.database().ref().update(updatesPost);
+    
+  });
+  
+  contPost.appendChild(textPost);
+  contPost.appendChild(btnUpdate );
+  contPost.appendChild(btnDelete);
+  posts.appendChild(contPost);
 })
 
 //salir de la cuenta del usuario
@@ -225,20 +297,17 @@ btnLogout.addEventListener('click', () => {
 
 //iniciando con google 
 btnGoogle.addEventListener("click", () => {
-
-  let provider = new firebase.auth.GoogleAuthProvider();
-
+  const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider)
     .then((result) => {
       console.log("ingrese con google");
-
+      const user = result.user;
+      writeUserData(user.uid, user.displayName, user.displayName, user.email, user.photoURL);
     })
     .catch((error) => {
-
       console.log(error.code);
       console.log(error.message);
       console.log(error.email);
       console.log(error.credential);
-
     });
 })
